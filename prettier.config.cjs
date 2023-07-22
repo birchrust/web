35b188dad27649a81a63a6ecfe6a9ad1116daa02
birch <121175143+birchrust@users.noreply.github.com>
2023-07-22 15:35:27 +0800

    Fix spelling.

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  bracketSpacing: true,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
 };
 
 module.exports = config;
