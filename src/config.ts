570c9c703a5a3a847d24d4c2f2c040c98aef140b
birch <121175143+birchrust@users.noreply.github.com>
2023-07-22 16:53:11 +0800

    feat: update config

---
+++ src/config.ts
@@ -0,0 +1,33 @@
+export const metadata = {
+  name: 'ThinKingChain',
+  author: 'birch',
+  title: 'birch',
+  description: 'this is desc',
+  openGraph: {
+    title: 'birch',
+    description: 'Accelerate your API development',
+    url: 'https://github.com/birchrust',
+    siteName: 'birchrust',
+  },
+  twitter: {
+    title: 'birch',
+    card: 'summary_large_image',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  links: {
+    twitter: '',
+    github: '',
+  },
+}
